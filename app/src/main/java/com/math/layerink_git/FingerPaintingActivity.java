95e9e76bee37db95259ccc164d59fb639366012c
mathilde <mathilde.grapin@gmail.com>
2017-02-25 14:40:50 +0100

    initial commit

---
+++ app/src/main/java/com/math/layerink_git/FingerPaintingActivity.java
@@ -0,0 +1,13 @@
+package com.math.layerink_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FingerPaintingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finger_painting);
+    }
+}
