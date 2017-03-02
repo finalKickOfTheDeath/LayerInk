cf52e3618afa47ee6234b026430dd10ddd5c1f8f
mathilde <mathilde.grapin@gmail.com>
2017-03-02 10:31:23 +0100

    ajout de l'activite principale

--- app/src/main/java/com/math/layerink_git/FingerPaintingActivity.java
+++ app/src/main/java/com/math/layerink_git/FingerPaintingActivity.java
@@ -1,13 +1,106 @@
 package com.math.layerink_git;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class FingerPaintingActivity extends AppCompatActivity {
 
+    private ImageButton btnMenu;
+    private ImageButton btnSave;
+    private ImageButton btnCamera;
+    private ImageButton btnGallery;
+    private ImageButton btnBrush;
+    private ImageButton btnReset;
+
+    DrawingView drawingView ;
+    private Paint paint;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_finger_painting);
-    }
+
+        // pour mettre l'activité en fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_fingerpaint);
+        getSupportActionBar().hide();
+
+        Log.d("deb", "ici ok1");
+
+        btnMenu = (ImageButton) findViewById(R.id.btnMenu);
+        btnSave = (ImageButton) findViewById(R.id.btnSave);
+        btnCamera = (ImageButton) findViewById(R.id.btnCamera);
+        btnGallery = (ImageButton) findViewById(R.id.btnGallery);
+        btnBrush = (ImageButton) findViewById(R.id.btnBrush);
+        btnReset = (ImageButton) findViewById(R.id.btnReset);
+
+        btnSave.setVisibility(View.INVISIBLE);
+        btnCamera.setVisibility(View.INVISIBLE);
+        btnGallery.setVisibility(View.INVISIBLE);
+        btnBrush.setVisibility(View.INVISIBLE);
+        btnReset.setVisibility(View.INVISIBLE);
+
+        Log.d("deb", "ici ok2");
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(12);
+
+
+        drawingView = (DrawingView) findViewById(R.id.drawingView);
+        drawingView.setPaint(paint);
+
+        Log.d("deb", "ici ok3");
+
+        btnMenu.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+                        Log.d("deb", "btnMenu touched!");
+
+                        if(btnSave.getVisibility() == View.INVISIBLE) {
+                            btnSave.setVisibility(View.VISIBLE);
+                            btnCamera.setVisibility(View.VISIBLE);
+                            btnGallery.setVisibility(View.VISIBLE);
+                            btnBrush.setVisibility(View.VISIBLE);
+                            btnReset.setVisibility(View.VISIBLE);
+                        } else {
+                            btnSave.setVisibility(View.INVISIBLE);
+                            btnCamera.setVisibility(View.INVISIBLE);
+                            btnGallery.setVisibility(View.INVISIBLE);
+                            btnBrush.setVisibility(View.INVISIBLE);
+                            btnReset.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                }
+        );
+
+        btnReset.setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        drawingView.clean();
+                    }
+                }
+        );
+
+        Log.d("deb", "ici ok5");
+
+    }// fin du onCreate
 }
